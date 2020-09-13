5b0c351588e571502ed54784d2cbc678767227eb
tefi-anbessa <frankrobinson99@hotmail.com>
2020-09-13 13:48:33 +0700

    Refactor palindrome to use HTML forms

--- bundle.js
+++ bundle.js
@@ -1,12 +1,25 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 let Phrase = require("tefi-palindrome");
-let string = prompt("Please enter a string for palindrome testing:");
-let test = new Phrase(string);
-if (test.palindrome()) {
-  alert(`Yay! "${test.content}" is a palindrome!`);
-} else {
-  alert(`No, "${test.content}" is not a palindrome.`);
-};
+
+function palindromeTester(event) {
+  event.preventDefault();
+
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `Yay! <strong>\<"${phrase.content}"\></strong> is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `Sorry, \<"${phrase.content}"\> is not a palindrome.`;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event) {
+    palindromeTester(event);
+  });
+});
 
 },{"tefi-palindrome":2}],2:[function(require,module,exports){
 module.exports = Phrase;
--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 let Phrase = require("tefi-palindrome");
-let string = prompt("Please enter a string for palindrome testing:");
-let test = new Phrase(string);
-if (test.palindrome()) {
-  alert(`Yay! "${test.content}" is a palindrome!`);
-} else {
-  alert(`No, "${test.content}" is not a palindrome.`);
-};
+
+function palindromeTester(event) {
+  event.preventDefault();
+
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `Yay! <strong>\<"${phrase.content}"\></strong> is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `Sorry, \<"${phrase.content}"\> is not a palindrome.`;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event) {
+    palindromeTester(event);
+  });
+});
